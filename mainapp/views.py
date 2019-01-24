d3f3b2052bdd759061ddde9a2ddf86dd6f57f8ef
OlDik <oldikarev@gmail.com>
2019-01-24 10:18:11 +0300

    настройка

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest
 
 
 # Create your views here.
@@ -7,8 +7,10 @@ from django.http import HttpResponse
 def main(request):
     return render(request, 'mainapp/index.html')
 
+
 def products(request):
     return render(request, 'mainapp/products.html')
+
 
 def contact(request):
     return render(request, 'mainapp/contact.html')
